a0454a8d80de40eb054584104cb30e2b8283e371
Bob Greenshields <bob@thegreenshields.com>
2010-11-17 20:14:07 +0000

    Added logging and commented out actions

--- authkeys/recipes/default.rb
+++ authkeys/recipes/default.rb
@@ -1,13 +1,22 @@
 
 auth_keys = node[:auth_keys]
 auth_keys.each do |key_data|
-	template "/home#{key_data["user"]}/.ssh/authorized_keys" do
-	  source "authorized_keys.erb"
-	  mode "0644"
-	  owner key_data["user"]
-	  group key_data["user"]
-	  variables(
-	    :keystrings => key_data["keys"]
-          )
-	end
+  kuser = key_data["user"]
+  Chef::Log.info("Key user is #{kuser}")
+#  directory "/home/#{kuser}/.ssh" do
+#    owner kuser
+#    group kuser
+#    mode "0700"
+#    action :create
+#  end
+  Chef::Log.info("#{kuser} has #{key_data["keys"].length} keys")
+#  template "/home#{kuser}/.ssh/authorized_keys" do
+#    source "authorized_keys.erb"
+#    mode "0644"
+#    owner kuser
+#    group kuser
+#    variables(
+#      :keystrings => key_data["keys"]
+#    )
+#  end
 end
