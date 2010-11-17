047970833c92df115b76ae63c11435cfad8784ab
Bob Greenshields <bob@thegreenshields.com>
2010-11-17 15:46:00 +0000

    Added auth_keys node data and array of keys per user

--- authkeys/recipes/default.rb
+++ authkeys/recipes/default.rb
@@ -1,9 +1,12 @@
-template "~/.ssh/authorized_keys" do
-  source "authorized_keys.erb"
-  mode
-  owner "root"
-  group "root"
-  variables(
-    :auth_keys => node[:auth_keys]
-  )
+
+auth_keys = node[:auth_keys]
+auth_keys.each do |key_data|
+	template "/home#{key_data["user"]}/.ssh/authorized_keys" do
+	  source "authorized_keys.erb"
+	  mode "0644"
+	  owner key_data["user"]
+	  group key_data["user"]
+	  variables(
+	    :keystrings => key_data["keys"]
+	end
 end
