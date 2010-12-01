5fdf3848657274334d8f2d6fcf3954fc35bf3fee
Bob Greenshields <bob@thegreenshields.com>
2010-12-01 16:07:01 +0000

    resolved boolean operator precedence

--- bobscode/providers/smbuser.rb
+++ bobscode/providers/smbuser.rb
@@ -102,7 +102,7 @@ def load_current_resource
 		Chef::Log.info("match2 is #{match2.inspect}")
 		match3 = info[3] =~ r_pwdunset
 		Chef::Log.info("match3 is #{match3.inspect}")
-		pwdunset = match2 and match3
+		pwdunset = (match2 and match3)
 		Chef::Log.info("pwdunset is #{pwdunset}")
 
 #		pwdunset = info[2] =~ r_pwdunset and info[3] =~ r_pwdunset
