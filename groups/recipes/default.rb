8b8933c76a717d4c8833cf22dda854e103712758
Bob Greenshields <bob@thegreenshields.com>
2010-12-02 11:24:53 +0000

    added testcmd for smbpasswd

--- groups/recipes/default.rb
+++ groups/recipes/default.rb
@@ -20,29 +20,9 @@
 if node[:groups]
 	groups = node[:groups]
 	groups.each do |g|
-#		Chef::Log::info g.inspect
 		group g["id"] do
 			gid g["gid"]
 			members g["members"]
 		end
 	end
 end
-
-#users = node[:users]
-#
-#users.each do |u|
-#
-#	home_dir = "/home/#{u['id']}"
-#
-#	user u['id'] do
-#		uid u['uid']
-#		gid "users"
-#		shell u['shell']
-#		comment u['comment']
-#		password u['password']
-#		if (u['home_dir'].upcase == "TRUE") then
-#			supports :manage_home => true
-#			home home_dir
-#		end
-#	end
-#end
