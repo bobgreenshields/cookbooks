b74c7361be2feb5ad6dc2289d99c3a2212207068
Bob Greenshields <bob@thegreenshields.com>
2010-12-02 14:49:42 +0000

    inserted stdout into u.split

--- bobscode/providers/smbuser.rb
+++ bobscode/providers/smbuser.rb
@@ -56,8 +56,8 @@ def load_current_resource
 #  u = shell_out("pdbedit -Lv -u #{new_resource.name}")
   u = shell_out("pdbedit -Lw -u #{new_resource.name}")
   Chef::Log.info("pdbedit yields #{u.inspect}")
-  info = [0, 1, 2, 3, "hello"]
-#  info = u.String::split(':')
+#  info = [0, 1, 2, 3, "hello"]
+  info = u.stdout.split(':')
   Chef::Log.info("info[0] is #{info[0]}")
   Chef::Log.info("info[1] is #{info[1]}")
   Chef::Log.info("info[2] is #{info[2]}")
