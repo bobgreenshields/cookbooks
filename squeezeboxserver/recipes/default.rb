b861dbddc64f10ea29119e487d1d8aac5327d780
Bob Greenshields <bob@thegreenshields.com>
2011-07-21 19:11:12 +0100

    add squeezeboxserver

---
+++ squeezeboxserver/recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: hosts
+# Recipe:: default
+#
+# Copyright 2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+  cookbook_file "/etc/apt/sources.list.d/debian-slimdevices.list" do
+    source "debian-slimdevices.list"
+    mode 00644
+    owner "root"
+    group "root"
+  end
+
+	package "squeezecenter" do
+		action :purge
+	end
+
+	execute "apt-get update" do
+	end
+
+	package "squeezeboxserver" do
+		action :install
+	end
