6a4474d1cea2dfbbddaa1851f5260001a487c767
Robert Greenshields <bobg@enterprise.(none)>
2011-08-24 08:38:20 +0100

    add pdftk recipe and start work on compiled vim

---
+++ vim/recipes/compile.rb
@@ -0,0 +1,5 @@
+depends = %w(libncurses5-dev libgnome2-dev libgnomeui-dev  libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev)
+
+# assumes ruby headers present from compile
+#scripts = %w(libperl-dev python-dev ruby-dev)
+scripts = %w(libperl-dev python-dev)
