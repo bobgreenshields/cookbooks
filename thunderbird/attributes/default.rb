b1d5a93198861321ec8623c3075be7a7853075e7
Robert Greenshields <bob@thegreenshields.com>
2012-04-25 11:50:57 +0100

    updated versions of nostalgy and zindus

--- thunderbird/attributes/default.rb
+++ thunderbird/attributes/default.rb
@@ -1,3 +1,3 @@
 default["thunderbird"]["addons"] =  %w(exteditor_v100.xpi 
-  nostalgy-0.2.27-tb+sm.xpi zindus-0.8.33-tb+sm.xpi)
+  nostalgy-0.2.30-tb+sm.xpi zindus-0.8.40-tb+sm+pb+sb.xpi)
 default["thunderbird"]["addon_dir"] =  "/home/bobg/tb-addons"
