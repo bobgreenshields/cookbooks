1c410b3269d57d6f3ddb7c32ced25c11ebed7800
Robert Greenshields <bob@thegreenshields.com>
2012-04-25 11:32:04 +0100

    add thunderbird::addons as seperate recipe

--- thunderbird/recipes/default.rb
+++ thunderbird/recipes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "thunderbird::addons"
 
 %w{thunderbird thunderbird-gnome-support}.each do |package|
 	apt_package package do
@@ -23,24 +24,3 @@
 	end
 end
 
-addon_dir = "/home/bobg/tb-addons"
-
-directory addon_dir do
-	owner "bobg"
-	group "bobg"
-	mode "0755"
-	action :create
-end
-
-addons = %w(exteditor_v100.xpi nostalgy-0.2.27-tb+sm.xpi
-	zindus-0.8.33-tb+sm.xpi)
-
-addons.each do |a|
-	cookbook_file "#{addon_dir}/#{a}" do
-		source a
-		mode "0755"
-		owner "bobg"
-		group "bobg"
-		action :create_if_missing
-	end
-end
