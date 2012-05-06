a8b7534577569ce359b92744daf9c0f2b797715e
Robert Greenshields <bob@thegreenshields.com>
2012-05-06 18:38:59 +0100

    add ux31 recipe

---
+++ ux31/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: hosts
+# Recipe:: default
+#
+# Copyright 2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# save sleep and hibernate file
+cookbook_file "/etc/pm/sleep.d/20_zenbook" do
+  source "20_zenbook"
+  owner "root"
+  group "root"
+  mode "0755"
+end
