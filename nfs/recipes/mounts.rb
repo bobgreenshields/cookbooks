2b78e60512debc23c97c45089adbc98e11f65e3e
Robert Greenshields <bob@thegreenshields.com>
2012-07-02 22:25:41 +0100

    correct do block

--- nfs/recipes/mounts.rb
+++ nfs/recipes/mounts.rb
@@ -1,16 +1,18 @@
 unless node["nfs"]["mounts"].empty?
   mounts = node["nfs"]["mounts"] 
-  mounts.each do |mount_point, v| do
+
+  mounts.each do |mount_point, v|
     directory mount_point do
     	owner v["owner"]
     	group v["group"]
     	mode v["mode"]
       recursive true
-    end
+    end # directory
+
     mount mount_point do
       device v["device"]
       fstype "nfs"
       options v["options"]
-    end
-  end
-end
+    end # mount
+  end # mounts.each
+end # unless
