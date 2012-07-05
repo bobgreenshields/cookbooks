607d5212112f3ded091b6769340859d17e065fd7
Robert Greenshields <bob@thegreenshields.com>
2012-07-05 19:38:21 +0100

    change ownership of root dirs

--- postfix/recipes/default.rb
+++ postfix/recipes/default.rb
@@ -32,17 +32,31 @@ if not node[:postfix].has_key?("required_mount") or
 	Regexp.new(node[:postfix][:required_mount]).match(`mount`)
 
 	mail_root = node[:postfix][:mail_folder]
+	mail_owner = node[:postfix][:mail_owner]
+	mail_group = node[:postfix][:mail_group]
 	folders = %w{cur new tmp .Drafts .Sent .Trash .Templates}
 	node[:postfix][:domains].each do |domain, dom_data|
+		directory "#{mail_root}/#{domain}" do
+			owner mail_owner
+			group mail_group
+			mode "0770"
+			action :create
+		end
 		dom_data["users"].each do |user|
+			directory "#{mail_root}/#{domain}/#{user["mail_folder"]}" do
+				owner mail_owner
+				group mail_group
+				mode "0770"
+				action :create
+			end
 			folders.each do |folder|
 				dir_arr = [] << mail_root << domain
 				dir_arr << user["mail_folder"] << folder
 				dir_name = dir_arr.join('/')
 #				puts "building dir #{dir_name}"
 				directory dir_name do
-					owner node[:postfix][:mail_owner]
-					group node[:postfix][:mail_group]
+					owner mail_owner
+					group mail_group
 					recursive true
 					mode "0770"
 					action :create
