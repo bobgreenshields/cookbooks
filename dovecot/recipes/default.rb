a32f653db6365a40d3a6bc8523d5710b9d416ca0
Robert Greenshields <bob@thegreenshields.com>
2012-07-06 10:56:45 +0100

    start dovecot recipy with template

---
+++ dovecot/recipes/default.rb
@@ -0,0 +1,13 @@
+%w{dovecot-common dovecot-imapd}.each do |p|
+	package p do
+		action :install
+	end
+end
+
+template "/etc/dovecot/passwd" do
+	source "passwd.erb"
+	mode "0660"
+	owner "root"
+	group "root"
+end
+
