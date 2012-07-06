82e6b63f0acd1d565eb2b71c25fd8924199860b5
Robert Greenshields <bob@thegreenshields.com>
2012-07-06 11:36:17 +0100

    restore template

--- dovecot/recipes/default.rb
+++ dovecot/recipes/default.rb
@@ -4,13 +4,17 @@
 	end
 end
 
+
+
+
+
 template "/etc/dovecot/passwd" do
 	source "passwd.erb"
 	mode "0660"
 	owner "root"
 	group "root"
-#	variables ({
-#		:domains => node[:dovecot][:domains]
-#	})
+	variables ({
+		:domains => node[:dovecot][:domains]
+	})
 end
 
