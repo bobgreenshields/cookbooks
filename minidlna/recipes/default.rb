861a564388216fd0d5d92f3f9e9d4e1db5d0fa2f
Robert Greenshields <bob@thegreenshields.com>
2012-07-07 10:00:49 +0100

    build array of media dir strings to pass to template

--- minidlna/recipes/default.rb
+++ minidlna/recipes/default.rb
@@ -2,3 +2,10 @@
 package "minidlna" do
 	action :install
 end
+
+dir_strs = node[:minidlna][:media_dirs].inject([]) do |arr, dir_data|
+	dir_str = "media_dir="
+	dir_str << dir_data.has_key?("restriction") ? "#{dir_data["restriction"]}," : ""
+	dir_str << dir_data["location"]
+	arr << dir_str
+end
