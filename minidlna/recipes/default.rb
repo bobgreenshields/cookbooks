c44ef69b64040b9224544c7bc201242f1089dc54
Robert Greenshields <bob@thegreenshields.com>
2012-07-07 10:07:45 +0100

    add template to recipe

--- minidlna/recipes/default.rb
+++ minidlna/recipes/default.rb
@@ -9,3 +9,14 @@ dir_strs = node[:minidlna][:media_dirs].inject([]) do |arr, dir_data|
 	dir_str << dir_data["location"]
 	arr << dir_str
 end
+
+template "/etc/minidlna.conf" do
+	source "minidlna.conf.erb"
+	owner "minidlna"
+	group "minidlna"
+	mode "0644"
+	variables ({
+		:dir_strs => dir_strs,
+		:friendly_name => node[:minidlna][:friendly_name]
+	})
+end
