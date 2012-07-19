1d9fc491b9716145f7f877c3389f5fbb9bd65fc0
Robert Greenshields <bob@thegreenshields.com>
2012-07-19 14:22:51 +0100

    add conditional execution

--- handbrake/recipes/default.rb
+++ handbrake/recipes/default.rb
@@ -1,6 +1,9 @@
 
 repo_name = 'stebbins/handbrake-releases'
 
+codename = node[:lsb][:codename]
+sources_file = "stebbins-handbrake-releases-#{codename}.list"
+
 execute "apt-get update" do
 	user "root"
 	action :nothing
@@ -9,6 +12,6 @@ end
 execute "add gnome-do repository" do
 	user "root"
 	command "add-apt-repository ppa:#{repo_name}"
-#	only_if do File.read(sources_file).select{|v| v =~ /#{repos}/}.length == 0 end
+	creates sources_file
 	notifies :run, "execute[apt-get update]", :immediately
 end
