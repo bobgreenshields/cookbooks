ecf4bc540103fcb343cebe2b3ec17caa116f9c48
Robert Greenshields <bob@thegreenshields.com>
2012-10-08 21:12:43 +0100

    added dovecot.conf and service to recipe

--- dovecot/recipes/default.rb
+++ dovecot/recipes/default.rb
@@ -4,9 +4,10 @@
 	end
 end
 
-
-
-
+service "dovecot" do
+	supports :restart => true, :start => true, :stop => true
+	action :nothing
+end
 
 template "/etc/dovecot/passwd" do
 	source "passwd.erb"
@@ -28,3 +29,21 @@ template "/etc/dovecot/users" do
 	})
 end
 
+template "/etc/dovecot/dovecot.conf" do
+	source "conf.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+	variables ({
+		:base_dir => node[:postfix][:base_dir],
+		:protocols => node[:postfix][:protocols],
+		:disable_plaintext_auth => node[:postfix][:disable_plaintext_auth],
+		:log_timestamp => node[:postfix][:log_timestamp],
+		:ssl_disable => node[:postfix][:ssl_disable],
+		:login_greeting => node[:postfix][:login_greeting],
+		:mail_location => node[:postfix][:mail_location],
+		:valid_chroot_dirs => node[:postfix][:valid_chroot_dirs],
+		:auth_default => node[:postfix][:auth_default]
+	})
+	notifies :restart, "service[dovecot]"
+end
