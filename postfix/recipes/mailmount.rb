be697f813c3ad7daed852db5c7f291f206f4cbdd
Robert Greenshields <bob@thegreenshields.com>
2012-10-14 19:57:05 +0100

    create mount point

--- postfix/recipes/mailmount.rb
+++ postfix/recipes/mailmount.rb
@@ -1,3 +1,12 @@
+mail_owner = node[:postfix][:mail_owner]
+mail_group = node[:postfix][:mail_group]
+directory "/mnt/mail" do
+	owner mail_owner
+	group mail_group
+	mode "0770"
+	action :create
+end
+
 mount "/mnt/mail" do
 	device "galactica:/mnt/secure/mail"
 	fstype "nfs"
