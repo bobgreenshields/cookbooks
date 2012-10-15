a5d815853d9d20f49fb0c83caab162dac2483f4c
Robert Greenshields <bob@thegreenshields.com>
2012-10-15 09:05:05 +0100

    start getmail recipe

---
+++ getmail/attributes/default.rb
@@ -0,0 +1,5 @@
+default["getmail"]["retrvr_type"] = "SimplePOP3Retriever"
+default["getmail"]["verbose_level"] = "2"
+default["getmail"]["read_all"] = "false"
+default["getmail"]["delete_after"] = "3"
+default["getmail"]["message_log "] = "/var/log/getmail/gm.log"
