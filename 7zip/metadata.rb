985850a65451a9317cdbfba7e74fafb7f2c66277
Bob Greenshields <bob@thegreenshields.com>
2015-03-01 21:08:50 +0000

    add metadata name

---
+++ 7zip/metadata.rb
@@ -0,0 +1,7 @@
+name             "7zip"
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Installs/Configures hosts"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
