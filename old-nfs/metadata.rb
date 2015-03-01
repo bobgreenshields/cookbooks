01a783a644a7d1ab8e19538789217d530f939ad5
bobgreenshields <bob@thegreenshields.com>
2015-03-01 22:27:09 +0000

    change README to .md

--- old-nfs/metadata.rb
+++ old-nfs/metadata.rb
@@ -3,5 +3,5 @@ maintainer       "Bob Greenshields"
 maintainer_email "bob@thegreenshields.com"
 license          "Apache 2.0"
 description      ""
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
